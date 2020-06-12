c99401efaa7758a0c811d52865b96b5be8257c32
Armino Popp <devel@boioiong.com>
2020-06-12 09:34:04 +0300

    Collapsible design ready

--- pages/index.js
+++ pages/index.js
@@ -114,36 +114,61 @@ export default function Home() {
               </div>
             </div>
           </section>
-          <p>
-             
-            
-            Frequently Asked Questions
-            Here are some of our FAQs.
-            If you have any other questions you’d like answered please feel free to email us.
-            What is Bookmark?
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          <section id="faq">
+            <div className="container faq-container">
+            <h2>Frequently Asked Questions</h2>
+              <p>Here are some of our FAQs.
+            If you have any other questions you’d like answered please feel free to email us.</p>
+            {/** TODO: make a collapsible component */}
+            <div className="collapsible-container">
+              <input id="item-0" className="toggle" type="checkbox" />
+              <label for="item-0" className="toggle-label">What is Bookmark?</label>
+              <div id="content-0" className="collapsible-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Fusce tincidunt justo eget ultricies fringilla.
-            Phasellus blandit ipsum quis quam ornare mattis.
-            How can I request a new browser?
-            Vivamus luctus eros aliquet convallis ultricies.
+            Phasellus blandit ipsum quis quam ornare mattis.</p>
+              </div>
+            <input id="item-1" className="toggle" type="checkbox" />
+              <label for="item-1" className="toggle-label">How can I request a new browser?</label>
+              <div id="content-1" className="collapsible-content">
+                <p>Vivamus luctus eros aliquet convallis ultricies.
             Mauris augue massa, ultricies non ligula.
             Suspendisse imperdiet.
             Vivamus luctus eros aliquet convallis ultricies.
             Mauris augue massa, ultricies non ligula.
             Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies.
             Mauris augue massa, ultricies non ligula.
-            Suspendisse imperdiet.
-
-            Is there a mobile app?
-            Sed consectetur quam id neque fermentum accumsan.
+            Suspendisse imperdiet.</p>
+              </div>
+            <input id="item-2" className="toggle" type="checkbox" />
+              <label for="item-2" className="toggle-label">Is there a mobile app?</label>
+              <div id="content-2" className="collapsible-content">
+                <p>Sed consectetur quam id neque fermentum accumsan.
             Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget.
             Cras in ligula quis est pharetra mattis sit amet pharetra purus.
-            Sed sollicitudin ex et ultricies bibendum.
-
-            What about other Chromium browsers?
-            Integer condimentum ipsum id imperdiet finibus.
+            Sed sollicitudin ex et ultricies bibendum.</p>
+              </div>
+            <input id="item-3" className="toggle" type="checkbox" />
+              <label for="item-3" className="toggle-label">What about other Chromium browsers?</label>
+              <div id="content-3" className="collapsible-content">
+                <p>Integer condimentum ipsum id imperdiet finibus.
             Vivamus in placerat mi, at euismod dui.
-            Aliquam vitae neque eget nisl gravida pellentesque non ut velit.
+            Aliquam vitae neque eget nisl gravida pellentesque non ut velit.</p>
+
+              </div>
+            </div>
+            </div>            
+          </section>
+          <p>
+            
+            
+            
+
+            
+            
+
+            
+            
             More Info
 
             35,000+ already joined
