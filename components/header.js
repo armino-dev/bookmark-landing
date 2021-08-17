091e90a77199d160ef96b6a58143ddb3df223a31
Armino Popp <devel@boioiong.com>
2021-08-17 15:42:00 +0300

    Upgrade to next@11.1.0 and react@17.0.2

--- components/header.js
+++ components/header.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Navigation from "../components/navigation";
 
 export default class Header extends React.Component {
--- components/navigation.js
+++ components/navigation.js
@@ -1,3 +1,4 @@
+import React from 'react'
 export default class Navigation extends React.Component {
     constructor(props) {
         super(props);
